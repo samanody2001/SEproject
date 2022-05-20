10443480ca35feaf5a231ef0f1e0adfa02f73805
samanody2001 <youssef.samanody@yahoo.com>
2022-05-20 03:02:00 +0200

    ekteb ay message

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const { MongoClient } = require("mongodb");
+const uri ="mongodb+srv://Samanody:%40Y145145@cluster0.bpe2tio.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
